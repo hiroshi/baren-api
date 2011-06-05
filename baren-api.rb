19672ef6e00fa03a98798104ee9bff32b0450740
Hiroshi Saito <hiroshi3110@gmail.com>
2011-06-05 11:40:11 +0900

    Initial commit

---
+++ baren-api.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'bundler/setup'
+require 'sinatra'
+require 'baren'
+require 'digest'
+require 'tmpdir'
+require 'json'
+
+def path_to_png(md5)
+  File.join(Dir.tmpdir, md5 + ".png")
+end
+
+#set :views, File.dirname(__FILE__)
+set :public, File.dirname(__FILE__) + "/public"
+
+get '/' do
+  redirect to("/demo.html")
+end
+
+post '/api/pjs' do
+  request.body.rewind  # in case someone already read it
+  pjs = request.body.read
+  md5 = Digest::MD5.hexdigest(pjs)
+  path = path_to_png(md5)
+  unless File.exist? path
+    Baren::PjsTemplate.new{ pjs }.render(nil).tap do |png|
+      File.open(path, "wb") do |f|
+        f.print png
+      end
+    end
+  end
+  if request.xhr?
+    content_type :json
+    {:url => to("/images/#{md5}.png")}.to_json
+  else
+    redirect to("/images/#{md5}.png")
+  end
+end
+
+get '/images/*.png' do |md5|
+  path = path_to_png(md5)
+  if File.exist? path
+    #content_type "image/png"
+    content_type :png
+    File.open(path, "rb").read
+  else
+    raise Sinatra::NotFound
+  end
+end
